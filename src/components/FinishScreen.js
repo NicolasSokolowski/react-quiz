6a3bdab330e5109f0e36d0a3c489913dda38ed7c
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-01-29 18:25:16 +0100

    useReducer traning

---
+++ src/components/FinishScreen.js
@@ -0,0 +1,24 @@
+import ResetButton from "./ResetButton";
+
+function FinishScreen({ points, maxPoints, highscore, dispatch }) {
+  const percentage = (points / maxPoints) * 100;
+
+  let emoji;
+  if (percentage === 100) emoji = "🥇";
+  if (percentage >= 80 && percentage < 100) emoji = "🎉";
+  if (percentage >= 50 && percentage < 80) emoji = "😊";
+  if (percentage >= 0 && percentage < 50) emoji = "🤔";
+  if (percentage === 0) emoji = "🤦‍♂️";
+
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span> You scored <strong>{points}</strong> out of {maxPoints} ({Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">(Highest score: {highscore} points)</p>
+      <ResetButton dispatch={dispatch}/>
+    </>
+  )
+}
+
+export default FinishScreen
